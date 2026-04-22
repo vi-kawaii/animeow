64610db25342f8b73c248abca74c4ec80584d81a
vi-wivi <velianjanjan@gmail.com>
2026-04-22 17:26:41 +0500

    add commit dialog to godot editor

---
+++ addons/git/git.gd
@@ -0,0 +1,22 @@
+@tool
+extends EditorPlugin
+
+var b
+
+func _enter_tree():
+	b = preload("button.tscn").instantiate()
+
+	var w = b.get_node("window")
+	w.hide()
+	w.close_requested.connect(func():
+		w.hide()
+	)
+
+	b.pressed.connect(func():
+		w.show()
+	)
+	add_control_to_container(CONTAINER_TOOLBAR, b)
+
+func _exit_tree():
+	remove_control_from_docks(b)
+	b.free()
