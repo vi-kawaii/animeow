964ea8c08dc525772fd69c7b8d854bdbdbb40129
vi-wivi <velianjanjan@gmail.com>
2026-05-18 02:57:12 +0500

    try to make game editor windowed

--- utils.gd
+++ utils.gd
@@ -2,6 +2,8 @@ extends Node
 
 const hostname = "http://localhost:3000"
 
+var once = true
+
 func fetch_get(url, callback):
 	var req = HTTPRequest.new()
 	add_child(req)
@@ -17,10 +19,18 @@ func target_area(area):
 	return str(area.get_parent().name) == "character" and area.get_parent().call("get_is_player")
 
 func game_editor():
-	return true
 	var args = OS.get_cmdline_user_args()
 
-	if args[0] == "--game-editor":
+	if args and args[0] == "--game-editor":
 		return true
 
+	toggle_to_fullscreen_mode_once()
 	return false
+
+func toggle_to_fullscreen_mode_once():
+	if not once:
+		return
+
+	once = false
+
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
