e819412046defe4f9ad21617cfa0328f2f992543
vi-kawaii <velianjanjan@gmail.com>
2026-06-24 13:59:42 +0500

    trying to fix errors appeared after folders removing

--- music_screen.gd
+++ music_screen.gd
@@ -23,6 +23,17 @@ func _ready():
 	load_tracks()
 
 func load_tracks():
-	tracks = ResourceLoader.list_directory("res://music")
-	%music_player.stream = AudioStreamWAV.load_from_file("res://music/%s" % tracks[0])
-	%music_buttons_container.visible = true
+	var all_files = ResourceLoader.list_directory("res://")
+	var music_files = []
+
+	# Фильтруем файлы по расширению .wav (или .mp3 / .ogg)
+	for file in all_files:
+		if file.ends_with(".wav") or file.ends_with(".wav.remap"):
+			music_files.append(file.get_basename()) # Получаем имя без .remap в экспортированной сборке
+
+	if music_files.size() > 0:
+		var track_path = "res://%s" % music_files[0]
+		%music_player.stream = AudioStreamWAV.load_from_file(track_path)
+		%music_buttons_container.visible = true
+	else:
+		print("Музыкальные файлы не найдены в корневой папке res://")
