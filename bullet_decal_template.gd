e6c7d8c39f333c41e24b1bcbaf616170f220d633
vi-kawaii <velianjanjan@gmail.com>
2026-07-03 23:39:19 +0500

    move decal to nowhere instead of toggle visible

--- bullet_decal_template.gd
+++ bullet_decal_template.gd
@@ -1,35 +1,22 @@
 extends Node3D
 
 var die_after = 3.
-
 var timer
 
 func _ready():
-	visible = false
-
 	timer = Timer.new()
 	add_child(timer)
 	timer.one_shot = true
 	timer.timeout.connect(_die)
 
+# Объект просто активирует то, что у него есть внутри, там, где он сейчас находится
 func impact():
-	_decal()
-	_effects()
-	_run_timer_to_die()
-
-func _decal():
-	visible = true
-	print("decal activated")
-
-func _effects():
 	$effect_from.restart()
 	$effect_from.emitting = true
-	print("cool effect from decal")
-
-func _run_timer_to_die():
-	print("some timer to delete decal")
 	timer.start(die_after)
 
 func _die():
-	print("decal animated and deactivated")
-	visible = false
+	teleport_to_abyss() # Объект сам убирает за собой, когда стал не нужен
+
+func teleport_to_abyss():
+	global_position = Vector3(99999, -99999, 99999)
--- bullet_decals_pool.gd
+++ bullet_decals_pool.gd
@@ -1,9 +1,7 @@
 extends Node3D
 
 var default_size = 10
-
 var bullet_decal_template = preload("res://bullet_decal_template.tscn")
-
 var array = []
 var i = 0
 
@@ -12,27 +10,19 @@ func _ready():
 		var b = bullet_decal_template.instantiate()
 		array.append(b)
 		add_child(b)
+		b.teleport_to_abyss() # Пул инициализирует «склад» в бездне
 
 func create(pos, normal):
-	print("spawned decal somewhere", pos, normal)
+	if normal.is_zero_approx(): return
 
-	# 1. Получаем текущую декаль по кругу с помощью модуля %
+	# 1. Менеджмент: выбираем объект
 	var b = array[i % array.size()]
-
-	# 2. Увеличиваем счетчик для следующего вызова
 	i += 1
 
+	# 2. Позиционирование: пул ставит объект в мир (ведь только пул знает pos и normal)
 	b.global_position = pos
-
-	# Проверяем на нулевую нормаль (если рейкаст улетел в небо)
-	if normal.is_zero_approx():
-		return
-
-	# Направляем ось Y (UP) декали по нормали стены
-	# Взгляд (FORWARD) направляем в любую другую сторону, например Vector3.FORWARD
-	# Если нормаль совпадает с FORWARD, используем UP
 	var forward_vector = Vector3.UP if abs(normal.z) > 0.9 else Vector3.FORWARD
-
 	b.global_transform = b.global_transform.looking_at(pos + forward_vector, normal)
 
+	# 3. Активация: приказ действовать
 	b.impact()
